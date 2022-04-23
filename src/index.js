f74f0b0f2e564cfd3ca2bdfe3ddc2692630347d7
jaab30 <jaab30@yahoo.com>
2022-04-23 09:25:47 -0400

    remove browser router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
 import './index.css';
 import App from './App';
 
 
-ReactDOM.render((
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <App />
-    </BrowserRouter>
-), 
-document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
